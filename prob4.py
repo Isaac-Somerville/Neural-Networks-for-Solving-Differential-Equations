52b7bee71f3b2d282a1ec0b8ec137ec6da6af994
Isaac Hayden <isaacshayden13@gmail.com>
2022-11-23 15:57:32 +0000

    coded prob 4, local minima problems

--- prob4.py
+++ prob4.py
@@ -28,140 +28,146 @@ class Fitter(torch.nn.Module):
         y = self.fc2(hidden)
         return y
     
-class DiffEq1():
+class DiffEq():
+    """
+    Differential equations from Lagaris et al. problem 4
+    This problem has a system of two coupled differential equations
+    D1(x) = 0 and D2(x) = 0
+    """
     def __init__(self, xrange, num_samples):
         self.xrange = xrange
         self.num_samples = num_samples
         
-    def solution1(x):
+    def solution1(self, x):
+        """Analytic solution to first DE"""
         return torch.sin(x)
     
-    def f1_trial(x,n1_out):
+    def f1_trial(self, x, n1_out):
+        """Trial solution f1(x) to first DE"""
         return x * n1_out
     
-    def df1_trial(x,n1_out,dn1dx):
+    def df1_trial(self, x, n1_out, dn1dx):
+        """Derivative of trial solution f1'(x) to first DE"""
         return n1_out + x*dn1dx
     
-    def solution2(x):
+    def solution2(self, x):
+        """Analytic solution to second DE"""
         return 1 + x**2
     
-    def f2_trial(x, n2_out):
+    def f2_trial(self, x, n2_out):
+        """Trial solution f2(x) to second DE"""
         return 1 + x*n2_out
     
-    def df2_trial(x, n2_out, dn2dx):
+    def df2_trial(self, x, n2_out, dn2dx):
+        """Derivative of trial solution f2'(x) to second DE"""
         return n2_out + x * dn2dx
     
-    #TODO: questa roba qui
-    def diffEq1(x, f1_trial, f2_trial, dn1dx):
-        return
+    def diffEq1(self, x, f1_trial, f2_trial, df1_trial):
+        """Returns D1(x) where first DE is D1(x) = 0"""
+        LHS = df1_trial
+        RHS = torch.cos(x) + f1_trial**2 + f2_trial - (1 + x**2 + torch.sin(x)**2)
+        return LHS - RHS
     
-    def diffEq2(x, f1_trial, f2_trial, dn2dx):
-        return
+    def diffEq2(self, x, f1_trial, f2_trial, df2_trial):
+        """Returns D2(x) where second DE is D2(x) = 0"""
+        LHS = df2_trial
+        RHS = 2*x - ((1+x**2)*torch.sin(x)) + (f1_trial*f2_trial)
+        return LHS - RHS
 
-def train(network, loader, loss_fn, optimiser, diffEq, epochs, iterations):
+def train(network1, network2, loader, loss_fn, optimiser, diffEq, epochs, iterations):
     """Trains the neural network"""
     cost_list=[]
-    network.train(True)
+    network1.train(True)
+    network2.train(True)
     for epoch in range(epochs+1):
         for batch in loader:
             x = batch.view(-1, 1)
-            n_out = network(x)
+            n1_out = network1(x)
+            n2_out = network2(x)
 
-            # Get the derivative of the network output with respect
+            # Get the derivative of both networks' outputs with respect
             # to the input values. 
-            dndx = torch.autograd.grad(n_out, x, torch.ones_like(n_out), retain_graph=True, create_graph=True)[0]
-            d2ndx2 = torch.autograd.grad(dndx, x, torch.ones_like(dndx), retain_graph=True, create_graph=True)[0]
+            dn1dx = torch.autograd.grad(n1_out, x, torch.ones_like(n1_out), retain_graph=True, create_graph=True)[0]
+            dn2dx = torch.autograd.grad(n2_out, x, torch.ones_like(n2_out), retain_graph=True, create_graph=True)[0]
+
+            # Get value of trial solutions f1(x), f2(x)
+            f1_trial = diffEq.f1_trial(x, n1_out)
+            f2_trial = diffEq.f2_trial(x, n2_out)
+            # Get df1 / dx and df2 / dx
+            df1_trial = diffEq.df1_trial(x, n1_out, dn1dx)
+            df2_trial = diffEq.df2_trial(x, n2_out, dn2dx)
+            # Get LHS of diff equation D1(x) = 0, D2(x) = 0
+            D1 = diffEq.diffEq1(x, f1_trial, f2_trial, df1_trial)
+            D2 = diffEq.diffEq2(x, f1_trial, f2_trial, df2_trial)
+            # Combine these into one tensor
+            D = torch.cat((D1,D2))
             
-            # Get value of trial solution f(x)
-            f_trial = trialFunc(x, n_out)
-            # Get df / dx
-            df_trial = dTrialFunc(x, n_out, dndx)
-            # Get d^2f / dx^2
-            d2f_trial = d2TrialFunc(x,n_out,dndx,d2ndx2)
-            # Get LHS of diff equation D(x) = 0
-            diff_eq = diffEq(x, f_trial, df_trial, d2f_trial)
-            
-            loss = loss_fn(diff_eq, torch.zeros_like(diff_eq))
+            # Calculate and store loss
+            loss = loss_fn(D, torch.zeros_like(D))
             cost_list.append(loss.detach().numpy())
+            
+            # Optimization algorithm
             loss.backward()
             optimiser.step()
             optimiser.zero_grad()
+            
         if epoch%(epochs/5)==0:
-            plotNetwork(network, solution,trialFunc, epoch, epochs, iterations)
-    network.train(False)
+            plotNetwork(network1, network2, diffEq, epoch, epochs, iterations)
+        
+    network1.train(False)
+    network2.train(False)
     return cost_list
 
 
-def plotNetwork(network, solution, trialFunc, epoch, epochs, iterations):
+def plotNetwork(network1, network2, diffEq, epoch, epochs, iterations):
+    """
+    Plots the outputs of both neural networks, along with the
+    analytic solution in the same range
+    """
     x    = torch.Tensor(np.linspace(xrange[0], xrange[1], num_samples)).view(-1,1)
-    N    = network.forward(x).detach().numpy()
-    exact = solution(x).detach().numpy()
-    plt.plot(x, trialFunc(x,N), 'r-', label = "Neural Network Output")
-    plt.plot(x, exact, 'b.', label = "True Solution")
+    N1   = network1.forward(x).detach().numpy()
+    N2   = network2.forward(x).detach().numpy()
+    exact1 = diffEq.solution1(x).detach().numpy()
+    exact2 = diffEq.solution2(x).detach().numpy()
+    
+    plt.plot(x, diffEq.f1_trial(x,N1), 'r-', label = "Neural Network 1 Output")
+    plt.plot(x, exact1, 'b.', label = "True Solution")
     
     plt.xlabel("x")
-    plt.ylabel("f(x)")
-    plt.legend(loc = "lower right")
-    plt.title(str(epoch + iterations*epochs) + " Epochs")
+    plt.ylabel("f1(x)")
+    plt.legend(loc = "lower center")
+    plt.title("Network 1: " + str(epoch + iterations*epochs) + " Epochs")
     plt.show()
     
+    plt.plot(x, diffEq.f2_trial(x,N2), 'r-', label = "Neural Network 2 Output")
+    plt.plot(x, exact2, 'b.', label = "True Solution")
     
-def solution3(x):
-    """
-    Analytic solution to Lagaris problem 3
-    """
-    return torch.exp(-x/5) * torch.sin(x)
-
-def trialFunc3(x, n_out):
-    """
-    Trial solution to Lagaris problem 3
-    f(x) = x + x^2 * N(x)
-    """ 
-    return x + (x**2 * n_out)
-
-def dTrialFunc3(x, n_out, dndx):
-    """
-    Derivative of trial solution to Lagaris problem 3
-    f'(x) = 1 + 2xN(x) + x^2 * N'(x)
-    """ 
-    return 1 + (2*x*n_out) + (x**2 * dndx)
-
-def d2TrialFunc3(x,n_out,dndx,d2ndx2):
-    """
-    Second derivative of trial solution to Lagaris problem 3
-    f''(x) = 2N(x) + (4x * N'(x)) + x^2 N''(x)
-    """ 
-    return 2*n_out + (4*x*dndx) + (x**2 * d2ndx2)
-
-def diffEq3(x, f_trial, df_trial, d2f_trial):
-    """
-    Returns LHS of differential equation D(x) = 0
-    for Lagaris problem 1
-    """
-    LHS = d2f_trial + (1/5)*df_trial + f_trial
-    RHS = -(1/5) * torch.exp(-x/5) * torch.cos(x)
-    return LHS - RHS
+    plt.xlabel("x")
+    plt.ylabel("f2(x)")
+    plt.legend(loc = "upper left")
+    plt.title("Network 2: " + str(epoch + iterations*epochs) + " Epochs")
+    plt.show()
     
-xrange=[0, 2]
+xrange=[0, 3]
 num_samples = 30
-network      = Fitter(num_hidden_nodes=10)
+diffEq = DiffEq(xrange, num_samples)
+network1     = Fitter(num_hidden_nodes=10)
+network2     = Fitter(num_hidden_nodes=10)
 train_set    = DataSet(num_samples,  xrange)
 train_loader = torch.utils.data.DataLoader(dataset=train_set, batch_size=60, shuffle=True)
 loss_fn      = torch.nn.MSELoss()
-optimiser    = torch.optim.Adam(network.parameters(), lr=1e-2)
-
-solution = solution3
-trialFunc = trialFunc3
-dTrialFunc = dTrialFunc3
-d2TrialFunc = d2TrialFunc3
-diffEq = diffEq3
+parameters = set()
+parameters |= set(network1.parameters())
+parameters |= set(network2.parameters())
+optimiser  = torch.optim.Adam(parameters, lr=1e-3)
 
 losses = [1]
 iterations = 0
 epochs = 5000
-while losses[-1] > 0.001 and iterations < 10:
-    losses.extend( train(network, train_loader, loss_fn, optimiser, solution, 
-                         trialFunc, dTrialFunc, d2TrialFunc, diffEq, epochs, iterations))
+while losses[-1] > 0.001  and iterations < 10:
+    newLoss = train(network1, network2, train_loader, loss_fn,
+                        optimiser, diffEq, epochs, iterations)
+    losses.extend(newLoss)
     iterations += 1
 losses = losses[1:]
 print(f"{iterations*epochs} epochs total, final loss = {losses[-1]}")
